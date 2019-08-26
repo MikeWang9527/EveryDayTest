9610aa9c28a98d2095d8fd7aefe78ea150e923e1
pengfei.wang <Cc123456>
2019-08-26 14:49:31 +0800

    8.26 每日

---
+++ src/LeetCode/Test_No48.java
@@ -0,0 +1,51 @@
+package LeetCode;
+
+/**
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ *
+ * 将图像顺时针旋转 90 度。
+ *
+ * 说明：你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ *
+ */
+public class Test_No48 {
+
+    //解法一 转置矩阵 +翻转   时间复杂度O(N^2)
+    public void rotate(int[][] matrix) {
+        int n  = matrix.length;
+
+        //转置
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+
+        //左右翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = tmp;
+            }
+        }
+    }
+
+
+    //解法二 在单次循环中旋转 4 个矩形
+    public void rotate2(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < (n + 1) / 2; i ++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
+
+}
