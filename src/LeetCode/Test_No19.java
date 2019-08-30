bdeef114f44700800de1c4d6453c9350ea2402b0
pengfei.wang <Cc123456>
2019-08-30 16:21:36 +0800

    8.30 每日

---
+++ src/LeetCode/Test_No19.java
@@ -0,0 +1,68 @@
+package LeetCode;
+
+/**
+ * 删除链表的倒数第N个节点
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点
+ */
+public class Test_No19 {
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    /**
+     * 两次遍历算法
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length = 0;
+        ListNode first = head;
+        while (first!=null){
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length>0){
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+
+    /**
+     * 一次遍历
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next =head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 1; i <= n+1; i++) {
+            first = first.next;
+        }
+
+        while (first!=null){
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+
+
+}
---
+++ src/LeetCode/Test_No237.java
@@ -0,0 +1,25 @@
+package LeetCode;
+
+/**
+ * 删除链表中的节点
+ */
+public class Test_No237 {
+
+    /**
+     * Definition for singly-linked list.
+     */
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode(int x) { val = x; }
+      }
+
+    /**
+     * 将下一个节点赋值 再删除节点
+     * @param node
+     */
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next =node.next.next;
+    }
+}
