539e4239c78c4a4e8555af9bc253379f535750ec
pengfei.wang <Cc123456>
2019-09-02 16:42:41 +0800

    9.2 每日

---
+++ src/LeetCode/Test_No206.java
@@ -0,0 +1,51 @@
+package LeetCode;
+
+/**
+ * 反转链表
+ */
+public class Test_No206 {
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    /**
+     * 迭代
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr!=null){
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    /**
+     * 递归
+     * @param head
+     * @return
+     */
+    public ListNode reverseList2(ListNode head) {
+        if (head==null||head.next==null){
+            return head;
+        }
+        ListNode p = reverseList2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+
+    }
+
+
+}
