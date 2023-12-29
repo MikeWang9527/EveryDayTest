80f869b4d3b6596eb207cba81f98b9c51b8e2121
Fenix.Wong <Fenix.Wong>
2023-12-29 11:48:35 +0800

    short 默认转int

---
+++ src/main/java/com/example/demo/StaticTest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+/**
+ * @author MikeWang
+ * @date 2023/12/29 11:35
+ */
+public class StaticTest {
+
+    public static  short returnShort(short s){
+        return s;
+    }
+
+    public static void main(String[] args) {
+        short s1 =5;
+        short s2 = 7;
+        //short s3 = s1+s2; //编译报错 int转short
+        returnShort(s1+s2);
+    }
+}
